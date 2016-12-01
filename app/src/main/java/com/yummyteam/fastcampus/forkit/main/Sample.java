d74f4bbd72860de9d41979b7c4aa537c76f63364
Dabin-PC\Dabin <gosoohro@gmail.com>
2016-12-01 16:00:17 +0900

    update 2016.12.01

--- app/src/main/java/com/yummyteam/fastcampus/forkit/main/Sample.java
+++ app/src/main/java/com/yummyteam/fastcampus/forkit/main/Sample.java
@@ -1,9 +1,13 @@
 package com.yummyteam.fastcampus.forkit.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.yummyteam.fastcampus.forkit.R;
+
+
 
 public class Sample extends AppCompatActivity {
 
@@ -11,5 +15,13 @@ public class Sample extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbarmenu, menu);
+        return true;
     }
 }
